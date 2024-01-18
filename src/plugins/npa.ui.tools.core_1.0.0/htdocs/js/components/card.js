1ea37a2b0cdb6b29bd39d9f225291938d5f599b6
Nicolas <Nicolas@MOEBIUS>
2024-01-18 18:02:09 +0100

    Fix bug

--- src/plugins/npa.ui.tools.core_1.0.0/htdocs/js/components/card.js
+++ src/plugins/npa.ui.tools.core_1.0.0/htdocs/js/components/card.js
@@ -3,11 +3,15 @@
  * Copyright 2023 Nicolas Renaudet - All rights reserved
  */
  
-function stretchWorkArea(){
-	$('#workArea').height($(window).height()-140);
-	$(window).on('resize',function(){
+function stretchWorkArea(height=-1){
+	if(height==-1){
 		$('#workArea').height($(window).height()-140);
-	});
+		$(window).on('resize',function(){
+			$('#workArea').height($(window).height()-140);
+		});
+	}else{
+		$('#workArea').height(height);
+	}
 }
  
 npaUiCore.Card = class Card extends NpaUiComponent{
@@ -26,13 +30,9 @@ npaUiCore.Card = class Card extends NpaUiComponent{
 				 child = $('#'+this.parentDivId+' div').first().detach();
 			}
 			let html = '';
-			let style='';
-			if(typeof config.height!='undefined'){
-				style += 'height: '+config.height+'px;';
-			}
-			html += '<div id="'+this.getId()+'" class="card" style="margin-left: 3px;margin-right: 3px;padding-top: 0px;padding-bottom: 0px;padding-left: 0px;'+style+'">';
+			html += '<div id="'+this.getId()+'" class="card" style="margin-left: 3px;margin-right: 3px;padding-top: 0px;padding-bottom: 0px;padding-left: 0px;">';
 			html += '  <div class="card-header npa-card-header">';
-			html += '    <img src="'+config.icon+'" width="20" style="padding-bottom: 3px;"><span id="scafPageTitle" style="padding-left: 15px;">'+config.label+'</span>&nbsp;&nbsp;&nbsp;<span class="visually-hidden spinner-border spinner-border-sm text-secondary" aria-hidden="true"></span>';
+			html += '    <img src="'+config.icon+'" width="20" style="padding-bottom: 3px;"><span id="scafPageTitle" style="padding-left: 15px;">'+this.getLocalizedString(config.label)+'</span>&nbsp;&nbsp;&nbsp;<span class="visually-hidden spinner-border spinner-border-sm text-secondary" aria-hidden="true"></span>';
 			html += '  </div>';
 			html += '  <ul class="list-group list-group-flush">';
 			html += '    <li id="workArea" class="list-group-item"></li>';
@@ -40,7 +40,11 @@ npaUiCore.Card = class Card extends NpaUiComponent{
 			html += '  <div id="statusBar" class="card-footer" style="padding-top: 0px;padding-bottom: 0px;padding-left: 5px;">&nbsp;</div>';
 			html += '</div>';
 			this.parentDiv().html(html);
-			stretchWorkArea();
+			if(typeof config.height!='undefined'){
+				stretchWorkArea(config.height);
+			}else{
+				stretchWorkArea();
+			}
 			if(child){
 				$('#workArea').append(child);
 			}
