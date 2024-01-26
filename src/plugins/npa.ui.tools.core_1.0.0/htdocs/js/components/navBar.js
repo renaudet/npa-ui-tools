61b8002fc110ba8fb064d2c20824d58e99786b67
Nicolas <Nicolas@MOEBIUS>
2024-01-26 12:56:33 +0100

    Adds support for dynamic menu and menu item placeholders

--- src/plugins/npa.ui.tools.core_1.0.0/htdocs/js/components/navBar.js
+++ src/plugins/npa.ui.tools.core_1.0.0/htdocs/js/components/navBar.js
@@ -6,6 +6,7 @@
 
 npaUiCore.NavBar = class NavBar extends NpaUiComponent{
 	actionIdToItem = {};
+	pluggableItems = {};
 	initialize(then){
 		$.loadCss('/uiTools/css/npaUiTheme.css',then);
 	}
@@ -26,6 +27,14 @@ npaUiCore.NavBar = class NavBar extends NpaUiComponent{
 		this.parentDiv().html(html);
 		var navBar = this;
 		this.generateNavBarMenu(function(){
+			for(var placeholderId in navBar.pluggableItems){
+				let items = navBar.pluggableItems[placeholderId];
+				for(var itemId in items){
+					let item = items[itemId];
+					let html = navBar.generateMenuItem(item);
+					$('#'+placeholderId).replaceWith(html);
+				}
+			}
 			$('.npa-navbar-menu').on('click',function(){
 				let actionId = $(this).data('actionid');
 				let item = navBar.actionIdToItem[actionId];
@@ -38,68 +47,101 @@ npaUiCore.NavBar = class NavBar extends NpaUiComponent{
 	}
 	generateNavBarMenu(then){
 		let config = this.getConfiguration();
-		if(typeof config.menus!='undefined'){
-			for(var i=0;i<config.menus.length;i++){
-				let menuConfig = config.menus[i];
-				let menu = '';
-				menu = this.generateMenu(menuConfig);
-				$('#navbarMenu_'+this.config.id).append(menu);
-			}
+		let navbar = this;
+		if(typeof config.providerUrl!='undefined'){
+			makeRESTCall('GET',config.providerUrl,{},function(response){
+				if(response.status==200){
+					navbar.generateUpperMenus(response.data);
+				}
+				navbar.generateUpperMenus(config.menus);
+				then();
+			},function(errorMsg){
+				console.log(errorMsg);
+				navbar.generateUpperMenus(config.menus);
+				then();
+			});
+		}else{
+			this.generateUpperMenus(config.menus);
 			then();
+		}
+	}
+	generateUpperMenus(upperMenuList){
+		if(typeof upperMenuList!='undefined' && upperMenuList!=null){
+			for(var i=0;i<upperMenuList.length;i++){
+				let menuConfig = upperMenuList[i];
+				$('#navbarMenu_'+this.config.id).append(this.generateMenu(menuConfig));
+			}
 		}
 	}
 	generateMenu(menuConfig){
 		let html = '';
-		if(typeof menuConfig.items!='undefined'){
-			html += '<li class="nav-item dropdown">';
-			html += '<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">';
-			html += this.getLocalizedString(menuConfig.label);
-			html += '</a>';
-			html += '<ul id="'+menuConfig.id+'" class="dropdown-menu" data-bs-popper="static">';
+		if(typeof menuConfig.type!='undefined' && 'placeholder'==menuConfig.type){
+			if(typeof this.pluggableItems[menuConfig.id]=='undefined'){
+				this.pluggableItems[menuConfig.id] = {};
+			}
 			for(var i=0;i<menuConfig.items.length;i++){
 				let item = menuConfig.items[i];
-				if('separator'==item.type){
-					html += '<li>';
-					html += '<hr class="dropdown-divider">';
-					html += '</li>';
-				}else{
-					let actionId = item.actionId;
-					if('redirect'==actionId){
-						actionId = item.id;
-					}
-					this.actionIdToItem[actionId] = item;
-					html += '<li id="'+item.id+'">';
-					html += '<a class="dropdown-item npa-navbar-menu" href="#" data-actionid="'+actionId+'">';
-					if(typeof item.icon!='undefined'){
-						var title = '';
-						if(typeof item.tooltip!='undefined'){
-							title = ' title="'+this.getLocalizedString(item.tooltip)+'"';
-						}
-						html += '<img src="'+item.icon+'" style="margin-right: 5px;" width="16"'+title+'>&nbsp;';
-					}
-					html += this.getLocalizedString(item.label);
-					html += '</a>';
-					html += '</li>';
-				}
+				this.pluggableItems[menuConfig.id][item.id] = item;
 			}
-			html += '</ul>';
-			html += '</li>';
 		}else{
-			this.actionIdToItem[menuConfig.actionId] = menuConfig;
-			html += '<li id="'+menuConfig.id+'" class="nav-item">';
-			html += '<a class="nav-link npa-navbar-menu" href="#" aria-current="page" data-actionid="'+menuConfig.actionId+'">';
-			if(typeof menuConfig.icon!='undefined'){
-				var title = '';
-				if(typeof menuConfig.tooltip!='undefined'){
-					title = ' title="'+this.getLocalizedString(menuConfig.tooltip)+'"';
+			if(typeof menuConfig.items!='undefined'){
+				html += '<li class="nav-item dropdown">';
+				html += '<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">';
+				html += this.getLocalizedString(menuConfig.label);
+				html += '</a>';
+				html += '<ul id="'+menuConfig.id+'" class="dropdown-menu" data-bs-popper="static">';
+				for(var i=0;i<menuConfig.items.length;i++){
+					let item = menuConfig.items[i];
+					html += this.generateMenuItem(item);
 				}
-				html += '<img src="'+menuConfig.icon+'" style="margin-right: 5px;" width="16"'+title+'>&nbsp;';
+				html += '</ul>';
+				html += '</li>';
+			}else{
+				this.actionIdToItem[menuConfig.actionId] = menuConfig;
+				html += '<li id="'+menuConfig.id+'" class="nav-item">';
+				html += '<a class="nav-link npa-navbar-menu" href="#" aria-current="page" data-actionid="'+menuConfig.actionId+'">';
+				if(typeof menuConfig.icon!='undefined'){
+					var title = '';
+					if(typeof menuConfig.tooltip!='undefined'){
+						title = ' title="'+this.getLocalizedString(menuConfig.tooltip)+'"';
+					}
+					html += '<img src="'+menuConfig.icon+'" style="margin-right: 5px;" width="16"'+title+'>&nbsp;';
+				}
+				html += this.getLocalizedString(menuConfig.label);
+				html += '</a>';
+				html += '</li>';
 			}
-			html += this.getLocalizedString(menuConfig.label);
+		}
+		return html;
+	}
+	generateMenuItem(item){
+		let html = '';
+		if('separator'==item.type){
+			html += '<li>';
+			html += '<hr class="dropdown-divider">';
+			html += '</li>';
+		}else
+		if('placeholder'==item.type){
+			html += '<li id="'+item.id+'"></li>';
+		}else{
+			let actionId = item.actionId;
+			if('redirect'==actionId){
+				actionId = item.id;
+			}
+			this.actionIdToItem[actionId] = item;
+			html += '<li id="'+item.id+'">';
+			html += '<a class="dropdown-item npa-navbar-menu" href="#" data-actionid="'+actionId+'">';
+			if(typeof item.icon!='undefined'){
+				var title = '';
+				if(typeof item.tooltip!='undefined'){
+					title = ' title="'+this.getLocalizedString(item.tooltip)+'"';
+				}
+				html += '<img src="'+item.icon+'" style="margin-right: 5px;" width="16"'+title+'>&nbsp;';
+			}
+			html += this.getLocalizedString(item.label);
 			html += '</a>';
 			html += '</li>';
 		}
-		
 		return html;
 	}
 }
