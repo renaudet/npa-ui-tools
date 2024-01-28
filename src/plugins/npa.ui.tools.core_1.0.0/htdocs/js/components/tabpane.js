24e1df998b15209b3938e46f02e9e4bf784b5f54
renaudet <nicolas.renaudet@wanadoo.fr>
2024-01-28 23:51:39 +0100

    Replace tab content with internal DIV (work in progress)

--- src/plugins/npa.ui.tools.core_1.0.0/htdocs/js/components/tabpane.js
+++ src/plugins/npa.ui.tools.core_1.0.0/htdocs/js/components/tabpane.js
@@ -9,25 +9,39 @@ npaUiCore.TabPane = class TabPane extends NpaUiComponent{
 	}
 	render(){
 		let config = this.getConfiguration();
-		let html = '';
-		html += '<ul class="nav nav-tabs" role="tablist">';
-		for(var i=0;i<config.tabs.length;i++){
-			let tab = config.tabs[i];
-			let active = true;
-			if(typeof tab.active!='undefined' && !tab.active){
-				active = false;
+		if(this.parentDiv().data('loaded')!='true'){
+			let childs = {};
+			if($('#'+this.parentDivId+' div').length>0){
+				$('#'+this.parentDivId+' div').each(function(){
+					childs[$(this).data('tab-target')] = $(this).detach();
+				});
+		    }
+			let html = '';
+			html += '<ul class="nav nav-tabs" role="tablist">';
+			for(var i=0;i<config.tabs.length;i++){
+				let tab = config.tabs[i];
+				let active = true;
+				if(typeof tab.active!='undefined' && !tab.active){
+					active = false;
+				}
+				html += '  <li class="nav-item" role="presentation">';
+				html += '    <button class="nav-link'+(i==0?' active':'')+'" id="'+tab.id+'" data-bs-toggle="tab" data-bs-target="#'+tab.id+'-pane" type="button" role="tab" aria-controls="'+tab.id+'-pane" aria-selected="'+(i==0?'true':'false')+'"'+(active?'':' disabled')+'>'+this.getLocalizedString(tab.label)+'</button>';
+				html += '  </li>';
 			}
-			html += '  <li class="nav-item" role="presentation">';
-			html += '    <button class="nav-link'+(i==0?' active':'')+'" id="'+tab.id+'" data-bs-toggle="tab" data-bs-target="#'+tab.id+'-pane" type="button" role="tab" aria-controls="'+tab.id+'-pane" aria-selected="'+(i==0?'true':'false')+'"'+(active?'':' disabled')+'>'+this.getLocalizedString(tab.label)+'</button>';
-			html += '  </li>';
+			html += '</ul>';
+			html += '<div class="tab-content" id="'+this.getId()+'_content">';
+			for(var i=0;i<config.tabs.length;i++){
+				let tab = config.tabs[i];
+				html += '  <div class="tab-pane fade show'+(i==0?' active':'')+'" id="'+tab.id+'-pane" role="tabpanel" aria-labelledby="'+tab.id+'" tabindex="0"></div>';
+			}
+			html += '</div>';
+			this.parentDiv().html(html);
+
+			for(var tabId in childs){
+				let div = childs[tabId];
+				$('#'+tabId+'-pane').empty();
+				$('#'+tabId+'-pane').append(div);
+			}
 		}
-		html += '</ul>';
-		html += '<div class="tab-content" id="'+this.getId()+'_content">';
-		for(var i=0;i<config.tabs.length;i++){
-			let tab = config.tabs[i];
-			html += '  <div class="tab-pane fade show'+(i==0?' active':'')+'" id="'+tab.id+'-pane" role="tabpanel" aria-labelledby="'+tab.id+'" tabindex="0">Content for '+this.getLocalizedString(tab.label)+'</div>';
-		}
-		html += '</div>';
-		this.parentDiv().html(html);
 	}
 }
